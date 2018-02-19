4f1b9414a17234ceb18fcdeb21a642d8fb81dcf1
Denis Rouzaud <denis.rouzaud@gmail.com>
2018-02-19 10:31:18 -0400

    docker run

--- scripts/ci/build-and-deploy.sh
+++ scripts/ci/build-and-deploy.sh
@@ -7,3 +7,5 @@ DIR=$(git rev-parse --show-toplevel)
 docker pull "qgis/qgis:${QGIS_VERSION}"
 
 docker build --build-arg QGIS_VERSION=${QGIS_VERSION} -t qgis/qgis-python-api-doc .
+
+docker run qgis/qgis-python-api-doc
