edbbda1d74d7bef0cbc1d73d047b2154fd0e5b1f
Denis Rouzaud <denis.rouzaud@gmail.com>
2018-02-19 10:41:45 -0400

    fix dir again

--- scripts/ci/build-and-deploy.sh
+++ scripts/ci/build-and-deploy.sh
@@ -4,8 +4,12 @@ set -e
 
 DIR=$(git rev-parse --show-toplevel)
 
+pushd ${DIR}
+
 docker pull "qgis/qgis:${QGIS_VERSION}"
 
 docker build --build-arg QGIS_VERSION=${QGIS_VERSION} -t qgis/qgis-python-api-doc .
 
 docker run qgis/qgis-python-api-doc
+
+popd
