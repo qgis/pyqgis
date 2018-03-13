c664175649d66d6521b64875fb95f91ec9590748
Denis Rouzaud <denis.rouzaud@gmail.com>
2018-03-13 13:52:09 -0400

    show signal signatures

--- autoautosummary.py
+++ autoautosummary.py
@@ -30,30 +30,6 @@ class AutoAutoSummary(Autosummary):
                 include_public = []
             items = []
 
-            print(obj)
-            members = []
-            main_documenter = get_documenter(obj, super(obj))
-            print(type(main_documenter))
-
-            # directive = directive('autodoc', 'English', doc)
-            # name = obj.__name__
-            # print(main_documenter(directive, name).filter_members(members, False))
-
-
-            # # https://stackoverflow.com/questions/3589311/get-defining-class-of-unbound-method-object-in-python-3/25959545#25959545
-            # def get_class_that_defined_method(meth):
-            #     if inspect.ismethod(meth):
-            #         for cls in inspect.getmro(meth.__self__.__class__):
-            #            if cls.__dict__.get(meth.__name__) is meth:
-            #                 return cls
-            #         meth = meth.__func__  # fallback to __qualname__ parsing
-            #     if inspect.isfunction(meth):
-            #         cls = getattr(inspect.getmodule(meth),
-            #                       meth.__qualname__.split('.<locals>', 1)[0].rsplit('.', 1)[0])
-            #         if isinstance(cls, type):
-            #             return cls
-            #     return getattr(meth, '__objclass__', None)  # handle special descriptor objects
-
             for name in dir(obj):
                 if name not in obj.__dict__.keys():
                     continue
