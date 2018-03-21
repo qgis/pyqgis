20e946e289c58e55bc4858da050e2aa459ff7823
Denis Rouzaud <denis.rouzaud@gmail.com>
2018-03-21 17:10:48 +0100

    fix docker tag

--- scripts/ci/build-and-deploy.sh
+++ scripts/ci/build-and-deploy.sh
@@ -6,9 +6,9 @@ DIR=$(git rev-parse --show-toplevel)
 
 pushd ${DIR}
 
-docker pull "qgis/qgis:${QGIS_VERSION_BRANCH}"
-
 QGIS_DOCKER_TAG=$(sed 's/master/latest/' <<< ${QGIS_VERSION_BRANCH})
+
+docker pull "qgis/qgis:${QGIS_DOCKER_TAG}"
 
 docker build --build-arg QGIS_DOCKER_TAG=${QGIS_DOCKER_TAG} -t qgis/qgis-python-api-doc .
 
