096b2f7bbbe16d818a61e6aa9c13a5916e9124d4
Denis Rouzaud <denis.rouzaud@gmail.com>
2018-03-24 13:09:41 +0100

    use QGIS version and not Docker tag

--- scripts/ci/build-and-deploy.sh
+++ scripts/ci/build-and-deploy.sh
@@ -3,11 +3,11 @@
 set -e
 set -x
 
-DOCKER_TAG="$1"
+QGIS_VERSION=$( sed 's/latest/master/' <<< "$1" )
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 THEME_PATH=$(${DIR}/../install_rtd_version_theme.sh | egrep '^Installed.*\.egg$' | sed 's/^Installed //')
 echo "Custom theme installed in ${THEME_PATH}"
 
-${DIR}/../build-docs.sh -v ${DOCKER_TAG} -t ${THEME_PATH}
+${DIR}/../build-docs.sh -v ${QGIS_VERSION} -t ${THEME_PATH}
