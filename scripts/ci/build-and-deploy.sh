80d361dd775c4a3a4aa349be520e7f7038702892
Denis Rouzaud <denis.rouzaud@gmail.com>
2018-03-26 20:51:56 +0200

    fix sed regex

--- scripts/ci/build-and-deploy.sh
+++ scripts/ci/build-and-deploy.sh
@@ -4,7 +4,7 @@ set -e
 
 # latest => master, final-3_0_2 => 3.0
 QGIS_DOCKER_TAG=$1
-QGIS_VERSION=$( sed 's/latest/master/; s/^final-(\d_\d)(_\d)?/\1/; s/_/./g' <<< $DOCKER_IMAGE )
+QGIS_VERSION=$( sed -r 's/latest/master/; s/^final-(\d_\d)(_\d)?/\1/; s/_/./g' <<< $DOCKER_IMAGE )
 echo "Building for ${QGIS_VERSION}"
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
