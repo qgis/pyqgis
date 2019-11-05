26a6c5c1afbd01b6c306d6c935e66f352ae56539
Denis Rouzaud <denis.rouzaud@gmail.com>
2019-11-05 15:13:07 +0100

    fix QGIS minor version with 2 digits

--- scripts/run-docker.sh
+++ scripts/run-docker.sh
@@ -25,7 +25,7 @@ else
 fi
 
 # latest => master, final-3_0_2 => 3.0
-QGIS_VERSION=$(${GP}sed -r 's/latest(_cosmic|_disco)?/master/; s/^(final|release)-([0-9]_[0-9])(_[0-9])?(_cosmic|_disco)?/\2/; s/_/./g' <<<$QGIS_DOCKER_TAG)
+QGIS_VERSION=$(${GP}sed -r 's/latest(_cosmic|_disco)?/master/; s/^(final|release)-([0-9]_[0-9]{1,2})(_[0-9])?(_cosmic|_disco)?/\2/; s/_/./g' <<<$QGIS_DOCKER_TAG)
 echo "QGIS Docker tag: ${QGIS_DOCKER_TAG}"
 echo "Building for QGIS: ${QGIS_VERSION}"
 
