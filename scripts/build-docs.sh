e480239b4d8470afd57a4cd9179d3e654491df45
Denis Rouzaud <denis.rouzaud@gmail.com>
2020-08-20 08:14:20 +0200

    fold output on GH wokrflows

--- scripts/build-docs.sh
+++ scripts/build-docs.sh
@@ -2,37 +2,37 @@
 
 set -e
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 pushd ${DIR}/..
 
 QGIS_VERSION=master
 while getopts "q:p:c:v:" opt; do
   case $opt in
-    v)
-      QGIS_VERSION=$OPTARG
-      ;;
-    q)
-      QGIS_BUILD_DIR=$OPTARG
-      ;;
-    p)
-      if [[ -z $PACKAGE ]]; then
-        PACKAGE="--package $OPTARG"
-      else
-        PACKAGE="$PACKAGE $OPTARG"
-      fi
-      ;;
-    c)
-      if [[ -z $CLASS ]]; then
-        CLASS="--class $OPTARG"
-      else
-        CLASS="$CLASS $OPTARG"
-      fi
-      ;;
-    \?)
-      echo "Invalid option: -$OPTARG" >&2
-      exit 1
-      ;;
+  v)
+    QGIS_VERSION=$OPTARG
+    ;;
+  q)
+    QGIS_BUILD_DIR=$OPTARG
+    ;;
+  p)
+    if [[ -z $PACKAGE ]]; then
+      PACKAGE="--package $OPTARG"
+    else
+      PACKAGE="$PACKAGE $OPTARG"
+    fi
+    ;;
+  c)
+    if [[ -z $CLASS ]]; then
+      CLASS="--class $OPTARG"
+    else
+      CLASS="$CLASS $OPTARG"
+    fi
+    ;;
+  \?)
+    echo "Invalid option: -$OPTARG" >&2
+    exit 1
+    ;;
   esac
 done
 shift $(expr $OPTIND - 1)
@@ -40,29 +40,25 @@ shift $(expr $OPTIND - 1)
 echo "QGIS VERSION: ${QGIS_VERSION}"
 echo "PACKAGE LIMIT: ${PACKAGE}"
 
-if [[ -n $QGIS_BUILD_DIR ]]; then
-  export PYTHONPATH=$PYTHONPATH:$QGIS_BUILD_DIR/output/python
+if [[ -n ${QGIS_BUILD_DIR} ]]; then
+  export PYTHONPATH=${PYTHONPATH}:$QGIS_BUILD_DIR/output/python
   #export PATH=$PATH:/usr/local/bin/:$QGIS_BUILD_DIR/build/output/bin
 fi
-export PYTHONPATH=$PYTHONPATH:${DIR}/..
-echo "setting PYTHONPATH $PYTHONPATH"
+export PYTHONPATH=${PYTHONPATH}:${DIR}/..
+echo "setting PYTHONPATH ${PYTHONPATH}"
 
-
-echo "travis_fold:start:make_api_rst"
-echo "make API RST ./scripts/make_api_rst.py $PACKAGE $CLASS -v $QGIS_VERSION"
+echo "##[group]make API RST ./scripts/make_api_rst.py ${PACKAGE} ${CLASS} -v ${QGIS_VERSION}"
 
 # see https://bugs.launchpad.net/ubuntu/+source/opencv/+bug/1890170?comments=all
 export LD_PRELOAD=/lib/x86_64-linux-gnu/libstdc++.so.6
 
-./scripts/make_api_rst.py $PACKAGE $CLASS -v ${QGIS_VERSION}
+./scripts/make_api_rst.py ${PACKAGE} ${CLASS} -v ${QGIS_VERSION}
 cp -r _templates api/${QGIS_VERSION}/_templates
-echo "travis_fold:end:make_api_rst"
+echo "##[endgroup]"
 
-echo "travis_fold:start:build_html"
-echo "build HTML"
+echo "##[group]Build HTML"
 make prepare QGISVERSION=${QGIS_VERSION}
 make html QGISVERSION=${QGIS_VERSION}
-echo "travis_fold:end:build_html"
-
+echo "##[endgroup]"
 
 popd
