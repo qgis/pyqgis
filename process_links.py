4941e9c7a57a74e0c25871bbf40d17e2bfb7942b
Denis Rouzaud <denis.rouzaud@gmail.com>
2021-05-28 11:23:27 +0200

    fix

--- process_links.py
+++ process_links.py
@@ -125,8 +125,8 @@ def process_signature(app, what, name, obj, options, signature, return_annotatio
 
 def skip_member(app, what, name, obj, skip, options):
     # skip monkey patched enums (base classes are different)
-    if hasattr(obj, '__objclass__') and issubclass(obj.__objclass__, enum.Enum):
-        if obj.baseClass != obj.__objclass__:
-            print(f'skipping old enum {name} (moved from {obj.__objclass__} to {obj.baseClass})')
-            return True
+    if hasattr(obj, '__objclass__') and hasattr(obj, 'baseClass') \
+            and issubclass(obj.__objclass__, enum.Enum) and obj.baseClass != obj.__objclass__:
+        print(f'skipping old enum {name} (moved from {obj.__objclass__} to {obj.baseClass})')
+        return True
     return skip
